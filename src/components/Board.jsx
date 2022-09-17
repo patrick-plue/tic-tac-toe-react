0094d05ec6f13433bd2f731ffbebe1f9c3c718ca
Patrick Pluequett <ppluequett@gmail.com>
2022-09-17 11:25:09 +0200

    add funtion for userinput

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,19 @@
 import { useState, useEffect } from 'react';
 
-function Field() {
-  return <div className="field"></div>;
+function Field({ i1, i2, isMarked, handleClick }) {
+  console.log(isMarked);
+  function displayMark() {
+    if (isMarked[i1][i2] === 1) {
+      return <p>x</p>;
+    } else if (isMarked[i1][i2] === 2) {
+      return <p>o</p>;
+    }
+  }
+  return (
+    <div className="field" onClick={() => handleClick(i1, i2)}>
+      {displayMark()}
+    </div>
+  );
 }
 
 export default function Board() {
@@ -10,10 +22,18 @@ export default function Board() {
     [0, 0, 0],
     [0, 0, 0],
   ]);
+
+  function setMark(index1, index2) {
+    const copyIsMarked = [...isMarked];
+    copyIsMarked[index1][index2] = 1;
+    setIsMarked(copyIsMarked);
+  }
   return (
     <div className="board">
       {isMarked.map((field, i1) =>
-        field.map((el, i2) => <Field i1={i1} i2={i2} />)
+        field.map((el, i2) => (
+          <Field i1={i1} i2={i2} isMarked={isMarked} handleClick={setMark} />
+        ))
       )}
     </div>
   );
