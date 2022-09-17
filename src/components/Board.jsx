70567f944c82d92787c9e9061f928be6ecce47d5
Patrick Pluequett <ppluequett@gmail.com>
2022-09-17 11:14:56 +0200

    change array structure

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,12 +5,16 @@ function Field() {
 }
 
 export default function Board() {
-  const [isMarked, setIsMarked] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [isMarked, setIsMarked] = useState([
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ]);
   return (
     <div className="board">
-      {isMarked.map((field) => (
-        <Field />
-      ))}
+      {isMarked.map((field, i1) =>
+        field.map((el, i2) => <Field i1={i1} i2={i2} />)
+      )}
     </div>
   );
 }
